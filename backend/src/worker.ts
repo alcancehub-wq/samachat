10d03fb22c31d3edb37290675bcace63b7bf5571
Samacon <alcancehub@gmail.com>
2026-06-16 19:55:05 -0300

    fix: respect worker run flag

--- backend/src/worker.ts
+++ backend/src/worker.ts
@@ -1,4 +1,4 @@
-import "dotenv/config";
+﻿import "dotenv/config";
 import "./bootstrap";
 import "./database";
 import { initRedis } from "./libs/redisStore";
@@ -6,8 +6,14 @@ import { logger } from "./utils/logger";
 import startScheduleWorker from "./services/ScheduleServices/RunScheduleWorker";
 import startCampaignWorker from "./services/CampaignServices/RunCampaignWorker";
 
-initRedis();
-startScheduleWorker();
-startCampaignWorker();
+const runWorkers = process.env.RUN_WORKERS !== "false";
 
-logger.info("Worker started");
+if (runWorkers) {
+  initRedis();
+  startScheduleWorker();
+  startCampaignWorker();
+
+  logger.info("Worker started");
+} else {
+  logger.warn("Worker disabled by RUN_WORKERS=false");
+}
